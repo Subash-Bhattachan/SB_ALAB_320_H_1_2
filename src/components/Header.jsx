5693499e93f70390a5a66dcbb37997e167434a86
Subash Bhattachan <bash_shoe@hotmail.com>
2024-12-06 03:11:28 -0500

    improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
+import Nav from './Nav';
 
 const Header = () => {
   return (
     <header>
     <h1 class="heading">Sartre's List</h1>
     <h3 class="saying">Better-Dressed People</h3>
-    <nav class="main-nav" aria-label="Main Navigation" role="navigation">
-      <ul>
-        <li><a href="#womens">Women's</a></li>
-        <li><a href="#mens">Men's</a></li>
-        <li><a href="#onthestreet">On the Street</a></li>
-        <li><a href="#catwalk">The Catwalk</a></li>
-        <li><a href="#adwatch">Adwatch</a></li>
-        <li><a href="#about">About</a></li>
-      </ul>
-    </nav>
+    <Nav />
   </header>
   );
 };
---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+
+        <nav class="main-nav" aria-label="Main Navigation" role="navigation">
+            <ul>
+                <li><a href="#womens">Women's</a></li>
+                <li><a href="#mens">Men's</a></li>
+                <li><a href="#onthestreet">On the Street</a></li>
+                <li><a href="#catwalk">The Catwalk</a></li>
+                <li><a href="#adwatch">Adwatch</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
+
+
+
+
+
+
+
+
+
